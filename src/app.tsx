cce654efdc762465198d42e51739001720c8b688
liujia02 <liujia02@renrenche.com>
2018-10-19 17:15:35 +0800

    添加图片

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import logo from './assets/logo.svg';
+import './style/app.scss';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
